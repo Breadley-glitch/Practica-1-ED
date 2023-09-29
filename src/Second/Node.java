f72e57b37eb1185db5312921e10569ccb92b5645
Breadley-glitch <124929511+Breadley-glitch@users.noreply.github.com>
2023-09-28 22:55:42 -0500

    Add new package and class List & Node

---
+++ src/Second/Node.java
@@ -0,0 +1,32 @@
+package Second;
+
+public class Node {
+ private Object data;
+ private Node next;
+ 
+ public Node() {
+	 data = null;
+	 next = null;
+ }
+
+ 
+ public Node(Object e) {
+	 
+ }
+ 
+ public Object getData() {
+	 return data;
+ }
+ 
+ public Node getNext() {
+	 return next;
+ }
+ 
+ public void setData(Object e) {
+	 this.data = e;
+ }
+ 
+ public void setNext(Node n) {
+	 this.next=n;
+ }
+}
