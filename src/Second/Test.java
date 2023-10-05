68ce76fc6998e13a6e1cfe145513e0142b90ca05
Breadley-glitch <124929511+Breadley-glitch@users.noreply.github.com>
2023-10-04 23:56:30 -0500

    test

---
+++ src/Second/Test.java
@@ -0,0 +1,46 @@
+package Second;
+
+public class Test {
+    public static void main(String[] args) {
+        // Create a new list
+        List myList = new List();
+
+        // Add elements to the list
+        myList.addFirst("First Element");
+        myList.addLast("Second Element");
+        myList.addLast("Third Element");
+        myList.addFirst("nine Element");
+        
+        System.out.println("All elements in the list:");
+        Node currentNode = myList.First();
+        while (currentNode != null) {
+            System.out.println(currentNode.getData());
+            currentNode = currentNode.getNext();
+        }
+
+        // Display the list size
+        System.out.println("List size: " + myList.size());
+
+        // Display the first and last elements
+        System.out.println("First element: " + myList.First().getData());
+        System.out.println("Last element: " + myList.Last().getData());
+
+        // Remove and display the first element
+        Object removedElement = myList.removeFirst();
+        System.out.println("Removed element: " + removedElement);
+
+        // Display the updated list size and first element
+        System.out.println("Updated list size: " + myList.size());
+        System.out.println("First element after removal: " + myList.First().getData());
+
+        // Remove and display the last element
+        removedElement = myList.removeLast();
+        System.out.println("Removed element: " + removedElement);
+
+        // Display the updated list size and last element
+        System.out.println("Updated list size: " + myList.size());
+        System.out.println("Last element after removal: " + myList.Last().getData());
+        
+        
+    }
+}
