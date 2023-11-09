8e9a94d8cd3c93699ac12b4f848e2b3deace8d57
JuanJoseBaena <124928411+JuanJoseBaena@users.noreply.github.com>
2023-11-08 20:16:43 -0500

    Test y usuario

---
+++ src/Third/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String[] args) {
+        ArbolBinarioBusqueda abb = new ArbolBinarioBusqueda();
+        abb.insertar(new Usuario("Juan", 10101013));
+        abb.insertar(new Usuario("Pablo", 10001011));
+        abb.insertar(new Usuario("Maria", 10101015));
+        abb.insertar(new Usuario("Ana", 1010000));
+        abb.insertar(new Usuario("Diana", 10111105));
+        abb.insertar(new Usuario("Mateo", 10110005));
+
+        // 
+    }
+}
---
+++ src/Third/Usuario.java
@@ -0,0 +1,20 @@
+package Third;
+public class Usuario {
+    String nombre;
+    int identificacion;
+
+    public Usuario(String nombre, int identificacion) {
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+    }
+
+    int calcularClave() {
+        int suma = 0;
+        int numero = identificacion;
+        while (numero != 0) {
+            suma = suma + numero % 10;
+            numero = numero / 10;
+        }
+        return suma;
+     }
+  }
